2259d74c657c2544e9b4570120edf4e1a5827e3e
Harsh Sharma <krish9515274@gmail.com>
2025-10-14 12:36:59 +0530

    Fixed vercel url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobportal-frontend.vercel.app',
+    origin:'https://job-portal-frontend-kappa-two.vercel.app',
     credentials:true
 }
 
